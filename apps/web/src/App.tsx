a983c4c77c620623a919893bcf0af80e99dacc66
Ricky Lee <ricklee1998@naver.com>
2025-03-14 20:58:19 +0900

    404 페이지 생성

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,7 +1,8 @@
 import "./App.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Home from "./pages/index";
 import Result from "./pages/Result";
+import NotFound from "./pages/404";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const queryClient = new QueryClient();
@@ -11,7 +12,9 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/result/" element={<Result />} />
+          <Route path="/result" element={<Result />} />
+          <Route path="/404" element={<NotFound />} />
+          <Route path="*" element={<Navigate replace to="/404" />} />
         </Routes>
       </BrowserRouter>
     </QueryClientProvider>
---
+++ apps/web/src/pages/404.tsx
@@ -0,0 +1,16 @@
+import { useNavigate } from "react-router-dom";
+import "./404.css";
+
+export default function NotFound() {
+  const navigate = useNavigate();
+
+  return (
+    <div className="not-found-container">
+      <h1 className="not-found-title">404</h1>
+      <p className="not-found-description">페이지를 찾을 수 없습니다</p>
+      <button className="home-button" onClick={() => navigate("/")}>
+        {"홈으로"}
+      </button>
+    </div>
+  );
+} 
